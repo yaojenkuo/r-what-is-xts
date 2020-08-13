a5681cebb23188ae003979209ded8df75789d53e
Yao-Jen Kuo <tonykuoyj@gmail.com>
2020-08-13 22:50:15 +0800

    Create demo.R

---
+++ demo.R
@@ -0,0 +1,16 @@
+librar(quantmod)
+
+# what is xts
+start_date <- "2018-01-01"
+getSymbols(Symbols = "AAPL", from = start_date) # create a AAPL xts object in global environment
+class(AAPL)
+# deconstruct xts
+xts_index <- index(AAPL)
+xts_coredata <- coredata(AAPL)
+class(xts_index)
+class(xts_coredata)
+as.numeric(xts_index)
+# matrix with column names
+colnames(xts_coredata)
+xts_coredata[1, 1]
+xts_coredata[1, "AAPL.Open"]
